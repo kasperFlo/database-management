0be79486d0e56894999cf655884dffddcd89d24a
Florian <20kasperbauefr2@gmail.com>
2022-12-04 00:41:14 -0500

    v1.1 optimizations

--- main.py
+++ main.py
@@ -5,26 +5,19 @@ with open ('Databases/databaseV2.json', 'r') as filee:
     currentDB = json.load(filee)
 everyKeys = [key[0] for key in currentDB[0].items()] # assume all keys are the same --> pull keys from first item of database
 
-
 def read(ui:str=None) -> dict: #returns data base
-    if ui == 'new':
-        with open ('Databases/databaseV2.json', 'r') as filee:
-            temp = json.load(filee)
-    else:
-        with open ('database.json', 'r') as filee:
-            temp = json.load(filee)
+    with open ('Databases/databaseV2.json', 'r') as filee:
+        temp = json.load(filee)
     return temp
-
+#print/read
 def printData(userImp:str='None',dataset:str=None): #print data formated by key and values (assume all keys are the same)
-    currentDB = read('new')
-
+    currentDB = read()
 #check what the user wants to check/read  : IE certain dicts or certain values of dicts
-
     if userImp in everyKeys:lookingKey = True  # ID,NAMES,CLASS
     elif len(userImp)==1 and userImp.isnumeric() and int(userImp) in range(len(everyKeys)) : # 1,2,3,4,5,6
         lookingKey = True ; userImp = everyKeys[int(userImp)] 
-    elif ( len(userImp)==1 and ((len(currentDB))-((ord(userImp))-65) in range(1,len(currentDB)+1))) : # A,B,C,D,E,F
-        lookingKey = False ; userImp = ord(userImp)-65
+    elif (len(userImp)==1 and ((len(currentDB))-((ord(userImp.upper()))-65) in range(1,len(currentDB)+1))) : # A,B,C,D,E,F
+        lookingKey = False ; userImp = ord(userImp.upper())-65
     else:lookingKey = None #NOTHING
 
     #x / y axis print statments
@@ -38,7 +31,6 @@ def printData(userImp:str='None',dataset:str=None): #print data formated by key 
 #user wants a certain COLUMN / KEY INFORMATION highlighed
         if lookingKey and currentKey == userImp:
             print(f" {Fore.RED}[{str(index).center(2)}] | {Fore.CYAN} {eF.LJspace(everyKeys[index])}: {' | '.join([eF.centerSpace(i[:15]) for i in map(str,everyItemOfKey)])} |{Fore.RESET}") 
-
 #user wants a certain ROW highlighted 
         elif lookingKey == False:
             print(f" {Fore.RED}[{str(index).center(2)}] │ {Fore.RESET}{eF.LJspace(everyKeys[index])} : ",end="")#print keys / num
@@ -50,15 +42,14 @@ def printData(userImp:str='None',dataset:str=None): #print data formated by key 
                     print(f"{eF.centerSpace(value[:15])}",end="")
                 print(" | ",end="")
             print()
-
 #user wants it just printed normally nothing highlighed
         else: print(f" {Fore.RED}[{str(index).center(2)}] | {Fore.RESET} {eF.LJspace(everyKeys[index])}: {' | '.join([eF.centerSpace(i[:15]) for i in map(str,everyItemOfKey)])} |") 
 #UPDATE/EDIT
 def update():
-    currentDB = read('new')
+    currentDB = read()
 
     printData('','v2')
-    space = input('What position would you like to spot to change (IE: A15 / 15A)> ') #'D15' 
+    space = input('What position would you like to spot to change (IE: A15 / 15A)> ') 
     newItem = input('Which would you to change this item to > ') 
 
     # COLLECT calculate which dataset / COLUMN to use
@@ -73,25 +64,22 @@ def update():
     with open ('Databases/databaseV2.json', 'w') as filee:    
         json.dump(currentDB,filee,indent=4)
     printData('','v2')
-
 #CREATE
 def create():
-    currentDB = read('new')
+    currentDB = read()
     newIValues = {i:input(f'What would you like to change {i} to > ') for i in everyKeys}
     currentDB.append(newIValues)
     with open ('Databases/databaseV2.json', 'w') as filee:   
         json.dump(currentDB,filee,indent=4)
     printData('','v2')
-
 #DELETE
 def delete():
-    currentDB = read('new')
+    currentDB = read()
     removeValue = ord(input('Which Row do you want to remove > ').upper())-65
     currentDB.pop(removeValue)
     with open ('Databases/databaseV2.json', 'w') as filee:   
         json.dump(currentDB,filee,indent=4)
     printData('','v2')
-
 #--init--#
 while True:
     try:
